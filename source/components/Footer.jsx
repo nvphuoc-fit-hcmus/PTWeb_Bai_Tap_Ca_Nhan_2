68ea59f2db1f41d1c645540f4a77d03d94ad3b93
22120285 <kaiphuocnguyen1906@gmail.com>
2025-12-14 17:39:32 +0700

    them component footer don gian

---
+++ source/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 dark:bg-gray-800 py-4 px-6 mt-auto">
+      <div className="max-w-7xl mx-auto">
+        <div className="text-gray-600 dark:text-gray-400 text-sm">
+          &lt;footer&gt;
+        </div>
+      </div>
+    </footer>
+  );
+}
